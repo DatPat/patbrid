ab9ccee1be20e05bd045340dc55840750c7646e3
DatPat <46565632+DatPat@users.noreply.github.com>
2020-05-09 04:51:22 +0200

    add magnet support to patbrid
    this commit adds magnet-file support to patbrid

---
+++ index.js
@@ -0,0 +1,44 @@
+const chokidar = require('chokidar')
+const RealDebridWatcher = require('./lib/watchers/real-debrid')
+const Aria2Downloader = require('./lib/downloaders/aria2')
+
+const {
+  REAL_DEBRID_API_KEY,
+  ARIA2_URL,
+  ARIA2_SECRET,
+  WATCH_DIR = '/watch',
+  WATCH_RATE = 5000
+} = process.env
+
+if (!REAL_DEBRID_API_KEY) {
+  console.log('[!] REAL_DEBRID_API_KEY env var is not set')
+
+  process.exit(-1)
+}
+
+if (!ARIA2_URL) {
+  console.log('[!] ARIA2_URL env var is not set')
+
+  process.exit(-1)
+}
+
+if (!ARIA2_SECRET) {
+  console.log('[!] ARIA2_SECRET env var is not set')
+
+  process.exit(-1)
+}
+
+// Create a downloader instance
+const downloader = new Aria2Downloader(ARIA2_URL, ARIA2_SECRET)
+
+// Create a watcher instance
+const watcher = new RealDebridWatcher(REAL_DEBRID_API_KEY, downloader.download)
+
+// Watch for new torrent files
+console.log(`[+] Watching '${WATCH_DIR}' for new torrents`)
+
+chokidar.watch(`${WATCH_DIR}/*.torrent`)
+  .on('add', path => watcher.addTorrent(path))
+
+// Check the torrent watch list every "WATCH_RATE" ms
+setInterval(() => watcher.checkWatchList(), WATCH_RATE)
